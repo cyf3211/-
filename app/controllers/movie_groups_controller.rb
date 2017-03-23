2c8f5323f8307f136f967398f670e9c536ffa78b
陈扬帆 <cyf3211@qq.com>
2017-03-23 12:20:30 +0800

    确定使用着权限

--- app/controllers/movie_groups_controller.rb
+++ app/controllers/movie_groups_controller.rb
@@ -1,5 +1,6 @@
 class MovieGroupsController < ApplicationController
-   before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+   before_action :find_movie_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @movie_groups = MovieGroup.all
   end
@@ -23,11 +24,9 @@ class MovieGroupsController < ApplicationController
   end
 
   def edit
-    @movie_group = MovieGroup.find(params[:id])
   end
 
   def update
-    @movie_group = MovieGroup.find(params[:id])
     if @movie_group.update(movie_group_params)
       redirect_to movie_groups_path, notice: "更新成功！"
     else
@@ -36,14 +35,19 @@ class MovieGroupsController < ApplicationController
   end
 
   def destroy
-    @movie_group = MovieGroup.find(params[:id])
     @movie_group.destroy
-    flash[:alert] = "删除成功！"
-    redirect_to movie_groups_path
+    redirect_to movie_groups_path, alert: "删除成功！"
   end
   private
 
   def movie_group_params
     params.require(:movie_group).permit(:title, :description)
   end
+
+  def find_movie_group_and_check_permission
+    @movie_group = MovieGroup.find(params[:id])
+    if current_user != @movie_group.user
+    redirect_to root_path, alert: "对不起，你没有权限"
+  end
+
 end
